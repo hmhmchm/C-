ec40795544fad2699e93b93a1e368ca363fbd071
최 현민 <hyeonminchoi5@gmail.com>
2020-05-27 19:38:15 +0900

    Data structure //tree

---
+++ Project_test/Project_test/DateStructure.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+//int tree[i] = {'\0\, ... '\0'};
+//Ãß°¡
+
+void preorder(int i) {
+	if (i > 0 && tree[i] != '\0') {
+		printf(" %c ", tree[i]);
+		preorder(get_left_child(i));
+		preorder(get_right_child(i));
+	}
+}
+
+void postorder(int i) {
+	if (i > 0 && tree[i] != '\0') {
+		postorder(get_left_child[i]);
+		postorder(get_right_child(i));
+		printf(" %c ", tree[i]);
+	}
+
+}
+
+void inorder(int i) {
+	if (i > 0 && tree[i] != '\0') {
+		inorder(get_left_child[i]);
+		printf(" %c ", tree[i]);
+		inorder(get_right_child(i));
+	}
+}
+
+int main() {
+
+}
